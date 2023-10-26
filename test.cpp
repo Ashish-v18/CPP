2d6792025c220696599160008c95a7488fda738f
Ashish Kumar <141670172+Ashish-v18@users.noreply.github.com>
2023-10-26 22:43:37 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    //  std::cout << "Hello world!"<<std::endl;
+    int x = 125;
+    x = x % 10;
+    cout<< x;
+
+    // vector < int > v;
+
+    // int n; // Number of elements to be entered by the user
+    // cout << "Enter the number of elements: ";
+    // cin >> n;
+
+    // cout << "Enter " << n << " elements: ";
+    // for (int i = 0; i < n; i++) {
+    //     int element;
+    //     cin >> element;
+    //     v.push_back(element); // Inserting elements in the vector
+    // }
+    //  v.pop_back();
+    //  v.push_back(99);
+
+    // cout << "the elements in the vector: ";
+    // for (auto it = v.begin(); it != v.end(); it++)
+    //     cout << * it << " ";
+    return 0;
+}
